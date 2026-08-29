1dbbd956e3deaa943c62ce7a963837030ce99aa7
SirLouen <sir.louen@gmail.com>
2026-08-29 23:34:51 +0200

    chore(mailkit): scaffold the module

---
+++ mailkit/doc.go
@@ -0,0 +1,4 @@
+// SPDX-License-Identifier: Apache-2.0
+
+// Package mailkit renders and sends application mail.
+package mailkit
---
+++ mailkit/go.mod
@@ -0,0 +1,5 @@
+module github.com/gopherium/framework/mailkit
+
+go 1.25.7
+
+toolchain go1.26.7
